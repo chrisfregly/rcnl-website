27b03f3a6765f26014c2f52e4a149d0a6ab82329
Chris Fregly <pro@chrisfregly.com>
2025-08-14 19:16:24 -0700

    update footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
 
   return (
     <>
-      <div className='mt-16 bg-white w-full px-8 py-8 flex flex-col justify-center items-center border-t-2 border-slate-200'>
+      <div className='mt-16 bg-white w-full px-8 pt-8 pb-4 flex flex-col justify-center items-center border-t-2 border-slate-200'>
         <div className='max-w-[72em] w-full flex items-center justify-between'>
           <Link to={"/"}>
             <Logo className="h-6 sm:h-7 md:h-8 fill-slate-900" />
@@ -23,7 +23,10 @@ function Footer() {
             <a href="https://github.com/rcnl-org" target="_blank" rel="noreferrer">
               <FaGithub className="text-xl fill-slate-700" />
             </a>
-            </div>
+          </div>
+        </div>
+        <div className="max-w-[72em] w-full flex justify-end">
+          <div className="text-xs font-thin mt-1">Built by <a className="underline" href="https://www.chrisfregly.com" target="_blank" rel="noreferrer">Chris Fregly</a></div>
         </div>
       </div>
     </>
